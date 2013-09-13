b74902439c158feb27b0751218cf21dbaad939f6
Rainbough Phillips <rainbough@gmail.com>
2013-09-13 09:59:53 -0500

    Add comments to tennis.rb methods

--- tennis.rb
+++ tennis.rb
@@ -2,6 +2,12 @@ module Tennis
   class Game
     attr_accessor :player1, :player2
 
+    # initializes new tennis game and creates two players
+    #
+    # designates player 1 as opponent of player 2
+    # designates player 2 as opponent of player 1
+    # 
+
     def initialize
       @player1 = Player.new
       @player2 = Player.new
@@ -9,6 +15,12 @@ module Tennis
       @player1.opponent = @player2
       @player2.opponent = @player1
     end
+
+    #sets won ball to player number passed in and increments points for that player by 1
+    #returns and error message if 1 or 2 is not passed in.
+    #returns incremented score of player 
+    #
+    #e.g. game.wins_ball(1) would add 1 to player1.points.
     
     def wins_ball(player_num)
       
@@ -25,9 +37,14 @@ module Tennis
   class Player
     attr_accessor :points, :opponent
 
+    #initialize new player and sets points value of the player to 0
+
     def initialize
       @points = 0
     end
+
+    #returns opponent variable for player
+    #expects opponent to be set by calling method
     def opponent
      @opponent
     end
